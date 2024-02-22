7899f4071daebdcca55f51b0a76696b6206c1cd0
Tanimodori <unknowner2014@gmail.com>
2024-02-22 16:49:46 +0800

    deps: pinia-plugin-persistedstate

--- packages/renderer/src/index.ts
+++ packages/renderer/src/index.ts
@@ -1,11 +1,13 @@
 import { createApp } from 'vue';
 import { ConfigProvider, App as AppComponent, Layout, Menu, Button, Tabs, Table } from 'ant-design-vue';
 import { createPinia } from 'pinia';
+import piniaPluginPersistedstate from 'pinia-plugin-persistedstate';
 import App from '@/App.vue';
 import router from '@/router';
 import i18n from './locales';
 
 const pinia = createPinia();
+pinia.use(piniaPluginPersistedstate);
 const app = createApp(App);
 
 app.use(pinia);
--- packages/renderer/src/store/settings.ts
+++ packages/renderer/src/store/settings.ts
@@ -1,47 +1,55 @@
 import { defineStore } from 'pinia';
 import { ref } from 'vue';
 
-export const useSettingsStore = defineStore('settings', () => {
-  const connection = ref({
-    host: 'localhost',
-    port: 24399,
-    startup: true,
-    retry: true,
-  });
+export const useSettingsStore = defineStore(
+  'settings',
+  () => {
+    const connection = ref({
+      host: 'localhost',
+      port: 24399,
+      startup: true,
+      retry: true,
+    });
 
-  const damageStyle = ref({
-    bgAlpha: 100,
-    bgColor: 'FFFFFF',
-    bgCornerSize: 0,
-    fontAlpha: 100,
-    fontColor: '000000',
-    fontSize: 16,
-  });
+    const damageStyle = ref({
+      bgAlpha: 100,
+      bgColor: 'FFFFFF',
+      bgCornerSize: 0,
+      fontAlpha: 100,
+      fontColor: 'FFFFFF',
+      fontSize: 16,
+      fontBorderCol: '000000',
+      fontBorderSize: 0,
+    });
 
-  const damageCols = ref({
-    order: [],
-  });
+    const damageCols = ref({
+      order: [],
+    });
 
-  const windowStyle = ref({
-    align: 'topleft',
-    paddingtop: 0,
-    paddingbottom: 0,
-    paddingleft: 0,
-    paddingright: 0,
-    width: 700,
-    height: 500,
-  });
+    const windowStyle = ref({
+      x: 0,
+      y: 0,
+      width: 700,
+      height: 500,
+    });
 
-  const shortcut = ref({
-    enabled: true,
-    key: 'Ctrl+Shift+Num0',
-  });
+    const shortcut = ref({
+      enabled: true,
+      key: 'Ctrl+Shift+Num0',
+    });
 
-  return {
-    connection,
-    damageStyle,
-    damageCols,
-    windowStyle,
-    shortcut,
-  };
-});
+    return {
+      connection,
+      damageStyle,
+      damageCols,
+      windowStyle,
+      shortcut,
+    };
+  },
+  {
+    persist: {
+      key: 'record',
+      storage: localStorage,
+    },
+  },
+);
