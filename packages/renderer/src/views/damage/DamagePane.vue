5a7dedd04c112dd5300eb08a163c132c88b98824
Tanimodori <unknowner2014@gmail.com>
2024-02-22 15:48:51 +0800

    feat: damage display

---
+++ packages/renderer/src/views/damage/DamagePane.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <span
+      v-for="player in validPlayers"
+      :key="player.id"
+      class="damage-text"
+    >
+      {{ player.totalDamage[player.totalDamage.length - 1] }}
+      <br />
+    </span>
+  </div>
+</template>
+
+<script lang="ts" setup>
+  import type { RecordState } from '@/store/record';
+  import { computed } from 'vue';
+  const props = defineProps<{
+    record: RecordState;
+  }>();
+
+  const validPlayers = computed(() => {
+    console.log(props.record.players);
+    return props.record.players.filter(player => player);
+  });
+</script>
+
+<style scoped lang="less">
+  .damage-text {
+    color: white;
+    font-size: 24px;
+    font-weight: bold;
+  }
+</style>
--- packages/renderer/src/views/damage/DamageView.vue
+++ packages/renderer/src/views/damage/DamageView.vue
@@ -1,1 +1,20 @@
-<template><div>/damage</div></template>
+<template>
+  <DamagePane
+    v-if="activeRecord"
+    :record="activeRecord"
+  />
+</template>
+
+<script lang="ts" setup>
+  import { useRecordStore } from '@/store/record';
+  import { computed } from 'vue';
+  import DamagePane from './DamagePane.vue';
+
+  const recordStore = useRecordStore();
+  const activeRecord = computed(() => {
+    if (recordStore.activeRecordId) {
+      return recordStore.records.find(record => record.id === recordStore.activeRecordId);
+    }
+    return undefined;
+  });
+</script>
