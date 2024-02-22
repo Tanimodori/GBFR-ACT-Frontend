0b211c355ddae7a95624183fa57546ce8a8a0c5c
Tanimodori <unknowner2014@gmail.com>
2024-02-22 13:24:11 +0800

    feat: unified dayjs

--- packages/renderer/src/utils/dayjs.ts
+++ packages/renderer/src/utils/dayjs.ts
@@ -7,22 +7,24 @@ dayjs.extend(relativeTime);
 dayjs.extend(localizedFormat);
 dayjs.extend(updateLocale);
 
-dayjs.updateLocale('en', {
-  relativeTime: {
-    future: 'in %s',
-    past: '%s ago',
-    s: '%ds',
-    m: '%dm',
-    mm: '%dm',
-    h: '%dh',
-    hh: '%dh',
-    d: '%dD',
-    dd: '%dD',
-    M: '%dM',
-    MM: '%dM',
-    y: '%dY',
-    yy: '%dY',
-  },
-});
+const langs = ['en', 'zh-Hans'];
+
+for (const lang of langs) {
+  dayjs.updateLocale(lang, {
+    relativeTime: {
+      s: '%ds',
+      m: '%dm',
+      mm: '%dm',
+      h: '%dh',
+      hh: '%dh',
+      d: '%dD',
+      dd: '%dD',
+      M: '%dM',
+      MM: '%dM',
+      y: '%dY',
+      yy: '%dY',
+    },
+  });
+}
 
 export default dayjs;
