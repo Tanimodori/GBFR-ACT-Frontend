1541fa2022ddaf2fcd5828d56052fa8ddef4df66
Tanimodori <unknowner2014@gmail.com>
2024-02-22 11:12:25 +0800

    feat: StatsTable

---
+++ packages/renderer/src/views/stats/StatsPane.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="wrapper">
+    <StatsTable :record="props.record"></StatsTable>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import type {RecordState} from '@/store/record';
+import StatsTable from './StatsTable.vue';
+
+const props = defineProps<{
+  record: RecordState;
+}>();
+</script>
---
+++ packages/renderer/src/views/stats/StatsTable.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>{{ props.record }}</div>
+</template>
+
+<script lang="ts" setup>
+import type {RecordState} from '@/store/record';
+
+const props = defineProps<{
+  record: RecordState;
+}>();
+</script>
