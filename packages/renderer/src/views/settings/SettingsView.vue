2bfa6171384d63ff3933af6a1802e89bd62474cf
Tanimodori <unknowner2014@gmail.com>
2024-02-23 14:31:33 +0800

    feat: connection settings

--- packages/renderer/src/views/settings/SettingsView.vue
+++ packages/renderer/src/views/settings/SettingsView.vue
@@ -5,10 +5,15 @@
       {{ $t('settings.locale.title') }}
     </a-divider>
     <LocaleSettings />
+    <a-divider orientation="left">
+      <ApiOutlined class="anticon" />
+      {{ $t('settings.connection.title') }}
+    </a-divider>
   </div>
 </template>
 
 <script lang="ts" setup>
   import GlobalOutlined from '~icons/ant-design/global-outlined';
+  import ApiOutlined from '~icons/ant-design/api-outlined';
   import LocaleSettings from './LocaleSettings.vue';
 </script>
