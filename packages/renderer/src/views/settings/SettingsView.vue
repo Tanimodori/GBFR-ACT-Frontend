19ad58db9fe15f5ecbe6cc622c0e3efa105a3259
Tanimodori <unknowner2014@gmail.com>
2024-02-23 23:42:58 +0800

    feat: about

--- packages/renderer/src/views/settings/SettingsView.vue
+++ packages/renderer/src/views/settings/SettingsView.vue
@@ -25,6 +25,15 @@
       {{ $t('settings.shortcut.title') }}
     </a-divider>
     <ShortcutSettings />
+    <a-divider orientation="left">
+      <GithubOutlined class="anticon" />
+      {{ $t('settings.about.title') }}
+    </a-divider>
+    <div class="about">
+      {{ $t('settings.about.mit') }}
+      <br />
+      {{ $t('settings.about.copyright') }}
+    </div>
   </div>
 </template>
 
@@ -34,6 +43,7 @@
   import BgColorsOutlined from '~icons/ant-design/bg-colors-outlined';
   import LayoutOutlined from '~icons/ant-design/layout-outlined';
   import KeyboardAltOutline from '~icons/material-symbols/keyboard-alt-outline';
+  import GithubOutlined from '~icons/ant-design/github-outlined';
   import LocaleSettings from './LocaleSettings.vue';
   import ConnectionSettings from './ConnectionSettings.vue';
   import StyleSettings from './StyleSettings.vue';
@@ -45,5 +55,10 @@
   .setting-view {
     height: 100%;
     overflow-y: auto;
+  }
+
+  .about {
+    padding-bottom: 16px;
+    text-align: center;
   }
 </style>
