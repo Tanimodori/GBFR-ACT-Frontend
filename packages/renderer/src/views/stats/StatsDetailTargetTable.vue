0ca9eca45725adfb9ab6fb2d1bcf40c758b97638
Tanimodori <unknowner2014@gmail.com>
2024-02-23 22:24:38 +0800

    feat: StatsDetailTargetTable

--- packages/renderer/src/views/stats/StatsDetailTargetTable.vue
+++ packages/renderer/src/views/stats/StatsDetailTargetTable.vue
@@ -1,1 +1,54 @@
-<template><div></div></template>
+<template>
+  <a-table :columns="columns" :data-source="rows" size="small" :pagination="false" />
+</template>
+
+<script setup lang="ts">
+  import type { PlayerState } from '@/store/record';
+  import type { TableColumnType } from 'ant-design-vue';
+  import { computed } from 'vue';
+  import { useI18n } from 'vue-i18n';
+
+  const props = defineProps<{
+    player: PlayerState;
+  }>();
+
+  interface StatsDetailTargetTableRow {
+    key: number;
+    name: string;
+    totalDamage: number;
+  }
+
+  const rows = computed(() => {
+    const rows: StatsDetailTargetTableRow[] = [];
+    for (let i = 0; i < props.player.targets.length; i++) {
+      const target = props.player.targets[i];
+      if (!target) continue;
+      rows.push({
+        key: target.id,
+        name: target.id.toString(16),
+        totalDamage: target.damage,
+      });
+    }
+    return rows;
+  });
+
+  const { t } = useI18n();
+  const columns: TableColumnType<StatsDetailTargetTableRow>[] = [
+    {
+      title: t('statsTable.name'),
+      dataIndex: 'name',
+      key: 'name',
+      sorter: (a, b) => a.key - b.key,
+      sortDirections: ['descend', 'ascend'],
+    },
+    {
+      title: t('statsTable.totalDamage'),
+      dataIndex: 'totalDamage',
+      key: 'totalDamage',
+      sorter: (a, b) => a.totalDamage - b.totalDamage,
+      sortDirections: ['descend', 'ascend'],
+      align: 'right',
+      customRender: ({ text }) => Number(text).toLocaleString(),
+    },
+  ];
+</script>
