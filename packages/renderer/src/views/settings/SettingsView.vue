a519ec22628701b81dcb2bca108e7a4fb2fa568b
Tanimodori <unknowner2014@gmail.com>
2024-02-23 17:21:32 +0800

    feat: LayoutSettings

--- packages/renderer/src/views/settings/SettingsView.vue
+++ packages/renderer/src/views/settings/SettingsView.vue
@@ -10,14 +10,21 @@
       {{ $t('settings.connection.title') }}
     </a-divider>
     <ConnectionSettings />
+    <a-divider orientation="left">
+      <LayoutOutlined class="anticon" />
+      {{ $t('settings.layout.title') }}
+    </a-divider>
+    <LayoutSettings />
   </div>
 </template>
 
 <script lang="ts" setup>
   import GlobalOutlined from '~icons/ant-design/global-outlined';
   import ApiOutlined from '~icons/ant-design/api-outlined';
+  import LayoutOutlined from '~icons/ant-design/layout-outlined';
   import LocaleSettings from './LocaleSettings.vue';
   import ConnectionSettings from './ConnectionSettings.vue';
+  import LayoutSettings from './LayoutSettings.vue';
 </script>
 
 <style scoped lang="less">
