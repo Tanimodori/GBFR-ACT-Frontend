7e0aa5ea851f73a36fd791b35fde0f748a3f523b
Tanimodori <unknowner2014@gmail.com>
2024-02-27 19:39:58 +0800

    fix: wrong timespan display at tab

--- packages/renderer/src/utils/dayjs.ts
+++ packages/renderer/src/utils/dayjs.ts
@@ -10,7 +10,22 @@ import 'dayjs/locale/zh-cn';
 import 'dayjs/locale/zh-hk';
 import 'dayjs/locale/zh-tw';
 
-dayjs.extend(relativeTime);
+const thresholds = [
+  { l: 's', r: 1 },
+  { l: 'ss', r: 59, d: 'second' },
+  { l: 'm', r: 1 },
+  { l: 'mm', r: 59, d: 'minute' },
+  { l: 'h', r: 1 },
+  { l: 'hh', r: 23, d: 'hour' },
+  { l: 'd', r: 1 },
+  { l: 'dd', r: 30, d: 'day' },
+  { l: 'M', r: 1 },
+  { l: 'MM', r: 11, d: 'month' },
+  { l: 'y' },
+  { l: 'yy', d: 'year' },
+];
+
+dayjs.extend(relativeTime, { thresholds });
 dayjs.extend(localizedFormat);
 dayjs.extend(duration);
 dayjs.extend(updateLocale);
@@ -21,6 +36,7 @@ for (const lang of langs) {
   dayjs.updateLocale(lang, {
     relativeTime: {
       s: '%ds',
+      ss: '%ds',
       m: '%dm',
       mm: '%dm',
       h: '%dh',
